94126266946c32110b6ad79bf5ebefe68bb853d2
doobix <doobix@gmail.com>
2018-03-24 16:34:08 -0700

    progress

---
+++ src/ChatIFrameView.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ChatIFrameView extends Component {
+  render() {
+    return (
+      <iframe></iframe>
+    );
+  }
+}
+
+export default ChatIFrameView;
