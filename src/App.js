bf9c6e40007b3cea4bdaa0bbec0a710d22511558
doobix <doobix@gmail.com>
2018-03-24 14:23:28 -0700

    add view components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import ListView from './ListView';
+import MapView from './MapView';
 import './App.css';
-import { Colors, Icon, InputWithButton } from 'watson-react-components';
+import { ButtonsGroup, Colors, Header, Icon, InputWithButton } from 'watson-react-components';
+
+const WATSON_API = 'http://firstnet18.herokuapp.com/api/speech-to-text/token';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      view: 'list',
       isRecording: false,
       stream: null,
+      location: '',
     };
   }
 
@@ -16,6 +22,10 @@ class App extends Component {
     let micButton = this.state.isRecording ? <Icon type="microphone" fill={Colors.red_50} /> : <Icon type="microphone" />;
     return (
       <div className="App">
+        <Header />
+
+        {this.renderTabs()}
+
         <button onClick={e => this.toggleRecording()}>
           {micButton}
         </button>
@@ -24,13 +34,59 @@ class App extends Component {
           onSubmit={(e) => {
             this.setState({ submitText: e.target.value });
           }}
-          placeholder="Input some text here"
+          onChange={(e) => this.changeLocation(e)}
+          placeholder="Enter a location"
+          value={this.state.location}
         />
+
+        {this.renderView()}
       </div>
     );
   }
 
-  toggleRecording() {
+  renderTabs() {
+    return (
+      <ButtonsGroup
+        type="radio"
+        name="radio-buttons"
+        onClick={e => this.changeView(e)}
+        buttons={[{
+          value: 'list',
+          id: 'list-button',
+          text: 'List View',
+          selected: this.state.view === 'list',
+        }, {
+          value: 'map',
+          id: 'map-button',
+          text: 'Map View',
+          selected: this.state.view === 'map',
+        }]}
+      />
+    );
+  }
+
+  changeView(event) {
+    this.setState({
+      view: event.target.value,
+    });
+  }
+
+  changeLocation(event) {
+    this.setState({
+      location: event.target.value
+    });
+  }
+
+  renderView() {
+    switch (this.state.view) {
+      case 'map':
+        return <MapView />;
+      default:
+        return <ListView />;
+    }
+  }
+
+  toggleRecording(e) {
     if (this.state.isRecording) {
       this.stopRecording();
     } else {
@@ -42,10 +98,12 @@ class App extends Component {
   }
 
   startRecording() {
-    fetch('/api/speech-to-text/token')
-    .then(function(response) {
-        return response.text();
-    }).then(function (token) {
+    fetch(WATSON_API, {
+      method: 'POST',
+    })
+    .then((response) => {
+      return response.text();
+    }).then((token) => {
       var stream = window.WatsonSpeech.SpeechToText.recognizeMicrophone({
         token: token,
         object_mode: false
@@ -53,23 +111,26 @@ class App extends Component {
 
       stream.setEncoding('utf8'); // get text instead of Buffers for on data events
 
-      stream.on('data', function(data) {
+      stream.on('data', (data) => {
         console.log(data);
+        this.setState({
+          location: data,
+        });
       });
 
-      stream.on('error', function(err) {
+      stream.on('error', (err) => {
         console.log(err);
       });
 
       this.setState({ stream });
-    }).catch(function(error) {
+    }).catch((error) => {
       console.log(error);
     });
   }
 
   stopRecording() {
     if (this.state.stream) {
-      this.state.stream.stop.bind(this.state.stream);
+      this.state.stream.stop();
     }
   }
 }
