b235eb77a50c5d85cad2417a5f70288d554936ab
Herman Chan <hermchan89@gmail.com>
2018-03-24 16:32:36 -0700

    arcgismap

---
+++ src/ArcGisMap.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import { loadModules } from 'esri-loader';
+
+const options = {
+  url: 'https://js.arcgis.com/4.6/'
+};
+
+const styles =  {
+  container: {
+    height: '100vh',
+    width: '100vw'
+  },
+  mapDiv: {
+    padding: 0,
+    margin: 0,
+    height: '100%',
+    width: '100%'
+  },
+}
+
+class ArcGisMap extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: 'loading'
+    }
+  }
+
+  componentDidMount() {
+    loadModules(['esri/Map', 'esri/views/MapView', 'esri/Graphic', 'esri/symbols/SimpleMarkerSymbol', 'esri/Color', 'esri/symbols/SimpleLineSymbol'], options)
+      .then(([Map, MapView, SimpleMarkerSymbol, Color, Graphic]) => {
+        const map = new Map({ basemap: "streets" });
+        const view = new MapView({
+          container: "viewDiv",
+          map,
+          zoom: 15,
+          center: [-122.445, 37.752]
+        });
+        // const fillSymbol = {
+        //   type: "simple-fill", // autocasts as new SimpleFillSymbol()
+        //   color: [227, 139, 79, 0.8],
+        //   outline: { // autocasts as new SimpleLineSymbol()
+        //   color: [255, 255, 255],
+        //   width: 1
+        //   }
+        // };
+        // const graphic = new Graphic({
+        //   symbol: fillSymbol
+        // });
+
+        view.then(() => {
+          this.setState({
+            // graphic,
+            map,
+            view,
+            status: 'loaded',
+          });
+        });
+      })
+    };
+
+
+  renderMap() {
+    if(this.state.status === 'loading') {
+      return <div>loading</div>;
+    }
+  }
+
+  render() {
+    console.log(this.state)
+    return(
+          <div style={styles.container}>
+            <div id='viewDiv' style={ styles.mapDiv } >
+              {this.renderMap()}
+            </div>
+          </div>
+    )
+  }
+}
+
+export default ArcGisMap;
